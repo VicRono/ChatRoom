8157633912cc78ad533b64e0f98d68f20fd851fb
unknown <v.rono@icloud.com>
2018-08-14 14:53:00 -0500

    client class worked

---
+++ Client/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        TcpClient client;
+        NetworkStream stream;
+        public string userId;
+        public Client(TcpClient Client, NetworkStream Stream)
+        {
+            client = Client;
+            userId = "Human";
+            stream = Stream;
+        }
+        public void Send(string Message)
+        {
+            byte[] message = Encoding.ASCII.GetBytes(Message);
+            stream.Write(message, 0, message.Count());
+        }
+        public string Recieve()
+        {
+            byte[] recievedMessage = new byte[256];
+            stream.Read(recievedMessage, 0, recievedMessage.Length);
+            UI.DisplayMessage(Encoding.ASCII.GetString(recievedMessage));
+            Console.WriteLine(recievedMessageString);
+            return recievedMessageString;
+        }
+    }
+}
